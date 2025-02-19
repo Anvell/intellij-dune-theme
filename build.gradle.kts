2dcfdd79eb966d1ade4dc4ffbbe4817f0d3fed9c
Denis Trotsenko <18591277+Anvell@users.noreply.github.com>
2025-02-19 22:08:00 +0100

    Migrate to Gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+    id("org.jetbrains.intellij") version "1.17.4"
+}
+
+group = "io.github.anvell"
+version = "2.0.5"
+
+repositories {
+    mavenCentral()
+}
+
+intellij {
+    version.set("2024.1.7")
+    type.set("IC")
+}
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    buildSearchableOptions {
+        enabled = false
+    }
+
+    patchPluginXml {
+        sinceBuild.set("241")
+        untilBuild.set("243.*")
+    }
+}
