fb6f4d5a567b9a9a96703b2dc3780c2c972f492f
Denis Trotsenko <18591277+Anvell@users.noreply.github.com>
2026-06-26 21:11:42 +0200

    Update Gradle setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,27 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.25"
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.intellij.platform")
 }
 
 group = "io.github.anvell"
 version = "3.3.0"
 
-repositories {
-    mavenCentral()
+dependencies {
+    intellijPlatform {
+        intellijIdea("2026.1.3")
+    }
 }
 
-intellij {
-    version.set("2024.1.7")
-    type.set("IC")
-    updateSinceUntilBuild.set(false)
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "241"
+        }
+    }
 }
 
 tasks {
-    withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-    }
-
     buildSearchableOptions {
         enabled = false
-    }
-
-    patchPluginXml {
-        sinceBuild.set("241")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,27 @@
+@file:Suppress("UnstableApiUsage")
+
+import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
+
 pluginManagement {
     repositories {
         mavenCentral()
         gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.jetbrains.intellij.platform.settings") version "2.17.0"
+}
+
+dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+
+    repositories {
+        mavenCentral()
+
+        intellijPlatform {
+            defaultRepositories()
+        }
     }
 }
 
